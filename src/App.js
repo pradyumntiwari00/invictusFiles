7031f0a43a7d4a05bc5369d9dc6d16b587b66d07
Pradyumn Tiwari <77263121+pradyumntiwari00@users.noreply.github.com>
2023-01-05 20:59:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import OurSponsers from './components/OurSponsers.js';
+import WhySponser from './components/WhySponser.js';
+function App() {
+  return (
+    <>
+    <OurSponsers/>
+    <WhySponser/>
+    </>
+  );
+}
+
+export default App;
